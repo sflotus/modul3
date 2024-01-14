5f71f82649051df6fb7372c4bf593e8e0900605f
Arthur <nguyenanhtuan2111996@gmail.com>
2024-01-14 10:36:54 +0700

    update CS

--- DB_CaseStudy/case_study.sql
+++ DB_CaseStudy/case_study.sql
@@ -190,12 +190,57 @@ VALUES
 (7, 2, 1, 2),
 (8, 2, 12, 2);
 
-
-
-
-
-
-
-
+-- 1.	Thêm mới thông tin cho tất cả các bảng có trong CSDL để có thể thoả mãn các yêu cầu bên dưới.
+-- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+ select * from nhan_vien where  ho_ten like ('H%') or  ho_ten like ('T%') or ho_ten like ('K%') and length(ho_ten)  <=15;
+ -- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+ select *,year(curdate()) - year(ngay_sinh)  as so_tuoi from khach_hang where year(curdate()) - year(ngay_sinh)  between 18 and 50 and (dia_chi like('%Đà Nẵng') or dia_chi like ('%Quảng Trị'));
+-- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+select kh.ma_khach_hang,kh.ho_ten, count(hd.ma_hop_dong) as so_lan_dat_phong 
+from khach_hang kh 
+join hop_dong hd on  kh.ma_khach_hang  = hd.ma_khach_hang 
+join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+where kh.ma_loai_khach = (select ma_loai_khach from loai_khach  where ten_loai_khach = 'Diamond')
+group by kh.ma_khach_hang,kh.ho_ten
+order by so_lan_dat_phong asc;
+-- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien cho tất cả các khách hàng đã từng đặt phòng. 
+select kh.ma_khach_hang,kh.ho_ten,lk.ten_loai_khach,hd.ma_hop_dong,dv.ten_dich_vu,hd.ngay_lam_hop_dong,hd.ngay_ket_thuc,sum(dvdk.gia*hdct.so_luong+dv.chi_phi_thue) as tong_tien
+from khach_hang kh
+left join hop_dong hd on kh.ma_khach_hang =hd.ma_khach_hang
+left join dich_vu dv on dv.ma_dich_vu=hd.ma_dich_vu
+left join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+left join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by kh.ma_khach_hang,hd.ma_hop_dong;
+--  6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+select dv.ma_dich_vu,dv.ten_dich_vu,  dv.dien_tich,dv.chi_phi_thue,ldv.ten_loai_dich_vu from dich_vu dv
+left join loai_dich_vu ldv on ldv.ma_loai_dich_vu=dv.ma_loai_dich_vu
+where  dv.ma_dich_vu in ( 
+	select ma_dich_vu from  hop_dong hd
+    where year(ngay_lam_hop_dong) <2021
+    ) ;
+-- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+select dv.ma_dich_vu,dv.ten_dich_vu,  dv.dien_tich,dv.chi_phi_thue,ldv.ten_loai_dich_vu,dv.dien_tich,dv.so_nguoi_toi_da from dich_vu dv
+left join loai_dich_vu ldv on ldv.ma_loai_dich_vu=dv.ma_loai_dich_vu
+where  dv.ma_dich_vu in(
+select ma_dich_vu from  hop_dong hd
+    where year(ngay_lam_hop_dong) =2020
+) and dv.ma_dich_vu not in(
+select ma_dich_vu from  hop_dong hd
+    where year(ngay_lam_hop_dong) =2021);
+-- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+ -- c1:
+ SELECT DISTINCT ho_ten
+FROM khach_hang;
+ -- c2:
+ select ho_ten from khach_hang group by ho_ten;
+ -- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+ select month(ngay_lam_hop_dong) as 'Thang', count(ma_hop_dong) as'so_lan_dat' from hop_dong where year(ngay_lam_hop_dong)=2021
+ group by month(ngay_lam_hop_dong) 
+ order by  thang;
+ -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem
+select hd.ma_hop_dong,hd.ngay_lam_hop_dong,hd.ngay_ket_thuc, hd.tien_dat_coc,ifnull(sum(hdct.so_luong),0) as so_luong_dich_vu_di_kem from hop_dong hd
+left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+group by hd.ma_hop_dong;
 
 
