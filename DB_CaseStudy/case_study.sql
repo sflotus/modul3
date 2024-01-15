1eab39ad02fb271f39e797ab00f23d773c8f6f83
Arthur <nguyenanhtuan2111996@gmail.com>
2024-01-15 21:19:34 +0700

    upload CS

--- DB_CaseStudy/case_study.sql
+++ DB_CaseStudy/case_study.sql
@@ -204,7 +204,7 @@ where kh.ma_loai_khach = (select ma_loai_khach from loai_khach  where ten_loai_k
 group by kh.ma_khach_hang,kh.ho_ten
 order by so_lan_dat_phong asc;
 -- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien cho tất cả các khách hàng đã từng đặt phòng. 
-select kh.ma_khach_hang,kh.ho_ten,lk.ten_loai_khach,hd.ma_hop_dong,dv.ten_dich_vu,hd.ngay_lam_hop_dong,hd.ngay_ket_thuc,sum(dvdk.gia*hdct.so_luong+dv.chi_phi_thue) as tong_tien
+select kh.ma_khach_hang,kh.ho_ten,lk.ten_loai_khach,hd.ma_hop_dong,dv.ten_dich_vu,hd.ngay_lam_hop_dong,hd.ngay_ket_thuc,ifnull(sum(dvdk.gia*hdct.so_luong)+dv.chi_phi_thue,0) as tong_tien
 from khach_hang kh
 left join hop_dong hd on kh.ma_khach_hang =hd.ma_khach_hang
 left join dich_vu dv on dv.ma_dich_vu=hd.ma_dich_vu
@@ -212,7 +212,8 @@ left join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
 left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
 left join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
 group by kh.ma_khach_hang,hd.ma_hop_dong;
---  6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+--  6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+-- chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
 select dv.ma_dich_vu,dv.ten_dich_vu,  dv.dien_tich,dv.chi_phi_thue,ldv.ten_loai_dich_vu from dich_vu dv
 left join loai_dich_vu ldv on ldv.ma_loai_dich_vu=dv.ma_loai_dich_vu
 where  dv.ma_dich_vu in ( 
@@ -242,5 +243,79 @@ FROM khach_hang;
 select hd.ma_hop_dong,hd.ngay_lam_hop_dong,hd.ngay_ket_thuc, hd.tien_dat_coc,ifnull(sum(hdct.so_luong),0) as so_luong_dich_vu_di_kem from hop_dong hd
 left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
 group by hd.ma_hop_dong;
+
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem
+-- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách 
+-- hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+                                                                                                                                                                     select hd.ma_hop_dong,
+nv.ho_ten as ho_ten_nhan_vien,
+kh.ho_ten as ho_ten_khach_hang,
+kh.so_dien_thoai,
+dv.ten_dich_vu,
+hd.tien_dat_coc,
+sum(hdct.so_luong) as so_luong_dich_vu_di_kem
+from hop_dong hd
+left join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+left join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
+left join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+left join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+where year(hd.ngay_lam_hop_dong) = 2020 and quarter(hd.ngay_lam_hop_dong) = 4 and hd.ngay_lam_hop_dong not in (
+select ngay_lam_hop_dong from hop_dong where year(ngay_lam_hop_dong) = 2021 and quarter(ngay_lam_hop_dong) in (1,2)
+)
+group by kh.ma_khach_hang,hd.ma_hop_dong;
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select dvdk.ma_dich_vu_di_kem,group_concat(dvdk.ten_dich_vu_di_kem),sum(hdct.so_luong) as so_lan_dat from  dich_vu_di_kem dvdk
+join  hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by dvdk.ma_dich_vu_di_kem
+having so_lan_dat = (select max(so_luong) from hop_dong_chi_tiet);
+
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select  hd.ma_hop_dong,ldv.ten_loai_dich_vu,dvdk.ten_dich_vu_di_kem,count(dvdk.ma_dich_vu_di_kem) as so_lan_su_dung
+from hop_dong_chi_tiet  hdct
+join hop_dong hd on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+group by hd.ma_hop_dong,ldv.ten_loai_dich_vu,dvdk.ten_dich_vu_di_kem
+having count(dvdk.ma_dich_vu_di_kem) =1
+;
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan,
+-- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, vt.ten_vi_tri,nv.so_dien_thoai, nv.dia_chi, count(hd.ma_hop_dong) as so_luong_hop_dong
+from nhan_vien nv
+left join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+left join trinh_do  td on td.ma_trinh_do = nv.ma_trinh_do
+left join vi_tri vt on vt.ma_vi_tri =nv.ma_vi_tri
+where year(hd.ngay_lam_hop_dong) between 2020 and 2021
+group by nv.ma_nhan_vien
+having so_luong_hop_dong <=3;
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+-- de cho viec back up du lieu thi k nen xoa database ma nen them 1 cot status vs 2 gia tri 1  hoac 0)
+alter table nhan_vien add column `status` bit default 1;
+-- tao stored procedure lay toan bo thong tin nhan vien
+DELIMITER $$
+create  procedure get_all_info_employee()
+begin
+select * from nhan_vien where status =1;
+end $$
+DELIMITER ;
+call get_all_info_employee();
+-- xoa nhan vien  => set status  =0
+SET SQL_SAFE_UPDATES = 0; -- disable safe update
+	-- dung not in
+update  nhan_vien nv set `status` = 0 where  nv.ma_nhan_vien not in(
+select ma_nhan_vien from hop_dong where year(ngay_lam_hop_dong) >=2021
+);
+call get_all_info_employee();
+	-- dung not exits
+
+
+
+
+
+
+
 
 
